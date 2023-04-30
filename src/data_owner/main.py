1232ae4b53c746150a406adfaf2704e954f15cfa
Guilhem CARLET <guilhem.carlet@epita.fr>
2023-04-30 19:02:36 +0200

    Added tests to figure out how to use the library

--- src/data_owner/main.py
+++ src/data_owner/main.py
@@ -1,1 +1,68 @@
-print("Hello world from Data Owner !")
+import time
+import json
+from mpyc.runtime import mpc
+
+async def prediction(a):
+    #Read poids.txt....
+    return a + 1
+
+
+mpc.run(mpc.start())
+print(mpc.parties)
+print(mpc.parties[0])
+
+my_age = int(input("Enter age:"))
+
+secint = mpc.SecInt(16)
+secret_number = secint(my_age)
+
+s = mpc.input(secret_number, senders=0)
+test = mpc.run(prediction(secret_number))
+
+
+# b = mpc.run(mpc.sum(s))
+# out = mpc.run(mpc.output(b))
+
+print(mpc.run(mpc.output(test)))
+
+# mpc.run(mpc.output(prediction(my_age)))
+
+# mpc.run(mpc.transfer(mpc.parties[0], my_age))
+
+# a = 0
+# receive_value = await parties[1].receive(a)
+
+# print(receive_value)
+
+
+
+# Découpe du secret et l'envoie à toute les autres
+# parties
+# our_age = mpc.input(secint(my_age))
+# Calcul sur les computing parties
+
+
+# Print du résultat
+# print(mpc.run(mpc.output(mpc.sum(our_age))))
+# print(mpc.run(mpc.output(mpc.max(our_age))))
+
+
+
+
+# mpc.input(mpc.SecInt())
+
+# print(''.join(mpc.run(mpc.transfer("Hello world !"))))
+mpc.run(mpc.shutdown())
+
+
+# print("Hello world from Data Owner !")
+
+
+# SecInt
+# mpc.input()
+# mpc.output()
+# mpc.sum()
+#
+# mpc.parties (nbr de parties connectés)
+#
+# print("Variable:", await mpc.output(test))
--- src/model_owner/main.py
+++ src/model_owner/main.py
@@ -1,1 +1,43 @@
-print("Hello world from Model Owner !")
+import time
+from mpyc.runtime import mpc
+
+
+def prediction(a, b, c):
+    #Read poids.txt....
+    return a + 1
+
+
+# 1 Récupérer les data du client
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+c = int(input("Enter c:"))
+
+secint_a = mpc.SecInt(16)(a)
+
+secint_b = mpc.SecInt(16)(b)
+
+secint_c = mpc.SecInt(16)(c)
+
+mpc.run(mpc.start())
+
+a2 = mpc.input(secint_a)
+b2 = mpc.input(secint_b)
+c2 = mpc.input(secint_c)
+
+mpc.run(prediction(secint_a, secint_b, secint_b))
+
+# print(prediction(mpc.output(mpc.sum(a2)),
+#                  mpc.output(mpc.sum(b2)),
+#                  mpc.output(mpc.sum(c2))))
+# mpc.run(mpc.output(mpc.sum(a)))
+
+
+# print("Hello world from Model Owner !")
+
+# print(''.join(mpc.run(mpc.transfer("Hello world !"))))
+
+# get l'input
+#call prediction ?
+
+# mpc.run(mpc.output(42))
+mpc.run(mpc.shutdown())
